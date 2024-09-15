b07be3b615ec77432722909660c5713041d355e9
neo-jayfeather <156867325+neo-jayfeather@users.noreply.github.com>
2024-09-15 11:03:37 -0400

    Update tkinter test.py

--- software/tkinter test.py
+++ software/tkinter test.py
@@ -1,3 +1,6 @@
+
+
+
 import tkinter as tk    # tkinter for gui elements
 import serial           # serial for comms with CNC
 import time             # timing delays
@@ -10,16 +13,42 @@ pts = np.array([[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]) #saved 4 point grid
 measurePts = [] # i should use this instead of pts since it's the same
 inc = 1 # movement increments (in mm)
 increments = [0.1, 1, 5, 10, 100] #movement map list
+curStr = '' # current string to write 
+currentState = "idle"
 
+def writeStr():
+    global curStr
+    while True:
+        if(curStr != ''):
+            s.write((curStr +"\r\n").encode())
+            curStr = ''
+        else:
+            time.sleep(0.0005)
+writeString = threading.Thread(target = writeStr)
+def getCurrPos():
+    global curStr
+    global currPosStr
+    while True:
+        if(curStr == ''):
+            curStr = '?'
+        while s.in_waiting != 0: # this may cause an infinite loop in a scenario I haven't covered
+            time.sleep(0.005) # need at least ~0.00333 second pause for all the bits to have been sent
+            grbl_out = s.readline()
+            if(grbl_out.decode().strip().split("|")[0] == "<Run" or grbl_out.decode().strip().split("|")[0] == "<Idle"): #get idle or running position (updates constantly)
+                currPosStr.set(grbl_out.decode().strip().split(":")[1].split("|")[0])
+                global currentState
+                if(grbl_out.decode().strip().split("|")[0] == "<Idle"):
+                    currentState = "idle"
+                elif(grbl_out.decode().strip().split("|")[0] == "<Run"):
+                    currentState = "run"
+getPos = threading.Thread(target=getCurrPos)
 def placeGroup(group): # for when you have a list of tkinter objects to place (the list should be 2d, i.e., x = [[tk.Button(---), x, y]])
     for x in range(len(group)):
         group[x][0].place(x = group[x][1], y = group[x][2])
 def set(command): # sets position (absoloute)
-    s.write((command + "\r\n").encode())# send bytes, append 13-10 or \r\n (same thing)
-    '''while s.in_waiting != 0:
-        grbl_out = s.readline()         # read byes for as long as there are bytes in the buffer
-        if(grbl_out[len(grbl_out)-2] == 13 and grbl_out[len(grbl_out)-1] == 10 and grbl_out.decode()[:2] == "ok"): #if = ok, print ok
-            print(grbl_out.decode().strip())'''
+    global curStr
+    #s.write((command + "\r\n").encode())# send bytes, append 13-10 or \r\n (same thing)
+    curStr = command
 def updatePos(xyz): # update relative position, xyz is a 1d array with 3 items [x,y,z]
     global currentPos
     currentPos = np.round(np.add(currentPos, xyz),4)
@@ -31,21 +60,10 @@ def updateInc(): # updates increments for the increment button
             inc = increments[(x + 1)%len(increments)]
             break
     btn_text.set(f"Increment : {inc}")
-def setPos(p):
-    global currentPos
+def setPos(p): # sets position for p1-p4 buttons
     global pts
     pts[p] = currentPos
     ptsText[p].set(f"P{p+1}\n({pts[p][0]},{pts[p][1]},{pts[p][2]})")
-def getCurrPos():
-    global currPosStr
-    while True:
-        s.write(b"?")
-        while s.in_waiting != 0: # this may cause an infinite loop in a scenario I haven't covered
-            time.sleep(0.05)
-            grbl_out = s.readline()
-            if(grbl_out.decode().strip().split("|")[0] == "<Run" or grbl_out.decode().strip().split("|")[0] == "<Idle"):
-                currPosStr.set(grbl_out.decode().strip().split(":")[1].split("|")[0])
-getPos = threading.Thread(target=getCurrPos)
 def calculateMatrix():
     global pts
     pts[4][0] = inputs[1][0].get("1.0","end-1c") # Y - split
@@ -72,19 +90,28 @@ def moveToPoints(points):
     for y in range(len(points[0])):
         for x in range(len(points)):
             set(f"Z{currentPos[2]+2}")
+            time.sleep(0.5)
             set(f"X{points[x][y][0]} Y{points[x][y][1]}")
+            time.sleep(0.5)
             set(f"Z{currentPos[2]-2}")
+            time.sleep(0.5)
+            print("moving")
     set(f"Z{currentPos[2]+2}")
     set("X0 Y0")
-gui = tk.Tk()
+
+
+
+gui = tk.Tk() # initialize gui
 gui.title("CNC GUI Controller")
-btn_text = tk.StringVar()
-currPosStr = tk.StringVar()
-currPosStr.set("0.000, 0.000, 0.000")
 gui.geometry("450x350")
+
+btn_text, currPosStr = tk.StringVar(), tk.StringVar() #texts with variable strings!
+currPosStr.set("0.000, 0.000, 0.000")
 ptsText = [tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar(), tk.StringVar()]
+
 for x in range(len(ptsText)):
     ptsText[x].set(f"P{x+1}")
+
 buttons = [
     [tk.Button(gui, text = "left" , command = lambda : updatePos([-inc, 0, 0]), height = 2, width = 5) , 25, 75], #6 movement buttons
     [tk.Button(gui, text = "up"   , command = lambda : updatePos([0, inc, 0]), height = 2, width = 5)   , 75, 25],
@@ -101,7 +128,7 @@ buttons = [
     [tk.Button(gui, text = "P2", command = lambda : updatePos(np.subtract(np.array(pts[1]),np.array(currentPos))), height = 1, width = 4), 72, 300],
     [tk.Button(gui, text = "P3", command = lambda : updatePos(np.subtract(np.array(pts[2]),np.array(currentPos))), height = 1, width = 4), 119, 300],
     [tk.Button(gui, text = "P4", command = lambda : updatePos(np.subtract(np.array(pts[3]),np.array(currentPos))), height = 1, width = 4), 166, 300],
-    [tk.Button(gui, textvariable = currPosStr, command = lambda : getCurrPos(), height = 2, width = 15), 300, 75], #get current position
+    [tk.Button(gui, textvariable = currPosStr, height = 2, width = 15), 300, 75], #get current position -> should be turned into a label 
     [tk.Button(gui, text = "CALC", command = lambda : calculateMatrix()), 300, 200] #calculate matrix
 ]
 inputs = [
@@ -119,4 +146,5 @@ placeGroup(buttons) #create buttons, inputs and labels
 placeGroup(inputs)
 placeGroup(labels)
 getPos.start()
+writeString.start()
 gui.mainloop()
